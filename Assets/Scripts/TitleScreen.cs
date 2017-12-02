f044d2324da4a3df95e856602086241ddcf6e7f6
Persikan <30988624+ThePersika@users.noreply.github.com>
2017-12-03 00:04:23 +0100

    Touched Up Title Screen

--- Assets/Scripts/TitleScreen.cs
+++ Assets/Scripts/TitleScreen.cs
@@ -7,9 +7,29 @@ public class TitleScreen : MonoBehaviour {
     [SerializeField]
     private string inGameSceneName;
 
+    [SerializeField]
+    private TextMesh highScore;
+    [SerializeField]
+    private GameObject pressAnyTo;
+    [SerializeField]
+    private float blinksPerSec;
+    private float timeToBlink;
+
+    private void Start()
+    {
+        highScore.text = "High Score: ";
+        highScore.text += (PlayerPrefs.HasKey("High Score")) ? PlayerPrefs.GetInt("High Score").ToString() : "0";
+    }
+
     private void Update()
     {
-        if (Input.anyKeyDown)
+        if (Input.anyKeyDown && !Input.GetMouseButton(0) && !Input.GetMouseButton(1) && !Input.GetMouseButton(2))
             SceneHandler.Instance().ChangeScene(inGameSceneName);
+
+        if (Time.time >= timeToBlink)
+        {
+            pressAnyTo.SetActive(!pressAnyTo.activeSelf);
+            timeToBlink = Time.time + 1 / blinksPerSec;
+        }
     }
 }
