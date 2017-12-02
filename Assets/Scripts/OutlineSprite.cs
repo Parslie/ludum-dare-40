274a40ab4769205279b7f62b3911e252170bb426
Persikan <30988624+ThePersika@users.noreply.github.com>
2017-12-02 22:31:16 +0100

    Add Outlines to Sprites

---
+++ Assets/Scripts/OutlineSprite.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class OutlineSprite : MonoBehaviour {
+
+    private SpriteRenderer spriteRenderer;
+    private SpriteRenderer[] outlines = new SpriteRenderer[4];
+
+    private void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+        outlines[0] = new GameObject("OutlineTop", typeof(SpriteRenderer)).GetComponent<SpriteRenderer>();
+        outlines[1] = new GameObject("OutlineRight", typeof(SpriteRenderer)).GetComponent<SpriteRenderer>();
+        outlines[2] = new GameObject("OutlineBottom", typeof(SpriteRenderer)).GetComponent<SpriteRenderer>();
+        outlines[3] = new GameObject("OutlineLeft", typeof(SpriteRenderer)).GetComponent<SpriteRenderer>();
+        
+        foreach (SpriteRenderer outline in outlines)
+        {
+            outline.transform.parent = transform;
+            outline.transform.position = transform.position;
+            outline.transform.localScale = spriteRenderer.size;
+            outline.color = Color.black;
+            outline.sortingOrder = spriteRenderer.sortingOrder - 5000;
+            outline.sortingLayerID = spriteRenderer.sortingLayerID;
+        }
+
+        outlines[0].transform.Translate(Vector2.up * 0.1f);
+        outlines[1].transform.Translate(Vector2.right * 0.1f);
+        outlines[2].transform.Translate(Vector2.down * 0.1f);
+        outlines[3].transform.Translate(Vector2.left * 0.1f);
+    }
+
+    private void Update()
+    {
+        foreach (SpriteRenderer outline in outlines)
+        {
+            outline.sprite = spriteRenderer.sprite;
+        }
+    }
+}
