cc7f0abfd01639b35af2bd9f0648e9eb36b8228f
Persikan <30988624+ThePersika@users.noreply.github.com>
2017-12-02 12:08:22 +0100

    Created SceneManager

---
+++ Assets/Scripts/SceneHandler.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneHandler : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject overlay, overlayMask;
+    private float overlaySize;
+    private float maskSize, maskTargetSize, maskSizeVel;
+
+    private string levelName;
+
+    private void Start()
+    {
+        overlaySize = overlay.transform.localScale.x;
+        maskSize = 0;
+        maskTargetSize = overlaySize;
+    }
+
+    private void Update()
+    {
+        maskSize = Mathf.SmoothDamp(maskSize, maskTargetSize, ref maskSizeVel, 0.25f);
+        overlayMask.transform.localScale = Vector2.one * maskSize;
+
+        if (levelName != "" && maskSize <= 0.05f)
+            LoadScene();
+    }
+
+    private void LoadScene()
+    {
+        SceneManager.LoadScene(levelName);
+    }
+
+    public void ChangeScene(string name)
+    {
+        maskSize = overlaySize;
+        maskTargetSize = 0;
+
+        levelName = name;
+    }
+}
