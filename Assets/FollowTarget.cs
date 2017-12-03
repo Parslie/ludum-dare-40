8ff6e3d571e9daacea679360deb4aeef8ea782c0
Persikan <30988624+ThePersika@users.noreply.github.com>
2017-12-03 10:47:11 +0100

    Added Checker-Pattern and Level Prefab

---
+++ Assets/FollowTarget.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowTarget : MonoBehaviour {
+
+    [SerializeField]
+    private Transform target;
+    [SerializeField]
+    private bool snap;
+
+    private void LateUpdate()
+    {
+        if (target)
+        {
+            Vector2 pos = target.position;
+            if (snap)
+            {
+                pos.x = Mathf.Round(pos.x);
+                pos.y = Mathf.Round(pos.y);
+            }
+            transform.position = pos;
+        }
+    }
+}
